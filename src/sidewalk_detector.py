f7efce6f6a2cff6f6686b6c510ebafea56e6a00d
fisherofearth <fisherofearth@gmail.com>
2016-04-30 08:45:39 -0700

    add files

--- src/sidewalk_detector.py
+++ src/sidewalk_detector.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
 
-import rospy
-import message_filters
+#######################################################################
+# Auther: Xi Yu (Fisher)
+# Date: 04/30/2016
+#
+# This node provides an openCV-based algorithm which detects sidewalk 
+#	from a real video record. This algorithm assume that the bottom 
+# 	center of an imageframe is always capturing sidewalk. This area is 
+#	set as reference of sidewalk. Similaritiy is computed between
+# 	different ROI and ref_ROI. The finnal detection result is computed
+#	by searching algorith based on similarities and Canny edges.
+# This 2-day programming challenge is set to output point cloud as 
+#	depth description for sidewalk and non-sidewalk. However, Python
+#	does not contain PCL API. So this task has not be done yet.
 
+#######################################################################
+
+import rospy
+import numpy as np
+
+import message_filters
 from sensor_msgs.msg import Image, PointCloud2
 
 from cv_bridge import CvBridge, CvBridgeError
-
 import cv2
-import numpy as np
 
-import sensor_msgs.point_cloud2 as pc2
-
-color_sidewalk_edge = (0,255,0)
-
-pub_color_img_raw = rospy.Publisher('/sidewalk_detector/color/image_raw',  Image, queue_size=10)
-#pub_depth_img_raw = rospy.Publisher('/sidewalk_detector/depth/image_raw',  Image, queue_size=10)
-pub_canny_edge = rospy.Publisher('/sidewalk_detector/color/canny_edge',  Image, queue_size=10)
-#pub_marker = rospy.Publisher('/sidewalk_detector/color/marker',  Image, queue_size=10)
-pub_depth_pts_in  = rospy.Publisher('/sidewalk_detector/depth/points_in',  PointCloud2, queue_size=10)
-pub_depth_pts_out = rospy.Publisher('/sidewalk_detector/depth/points_out', PointCloud2, queue_size=10)
+#import sensor_msgs.point_cloud2 as pc2
 
 
-bridge = CvBridge()
-similarities_history = []
-num_step = 20 	# 10 : 2 : 30
+step_number = 20 	# 10 : 2 : 30
 T_edge = -0.08 	# -1.5 : 0.1 : -0.5
-T_edge_hp = 0.7 # 0.20 : 0.01 : 1.00
+T_similar = 0.7 # 0.20 : 0.01 : 1.00
 
 kernel_sharpen = np.array([[-1,-1,-1,-1,-1],
 							[-1,2,2,2,-1],
@@ -34,22 +38,36 @@ kernel_sharpen = np.array([[-1,-1,-1,-1,-1],
 							[-1,2,2,2,-1],
 							[-1,-1,-1,-1,-1]]) / 8.0
 
+similarities_history = [] # used for mean filter
+color_sidewalk = (0,255,0)
+bridge = CvBridge() # convert between openCV image and ImageMsgs
 
+### Define publisher
+pub_color_img_raw = rospy.Publisher('/sidewalk_detector/color/image_raw',  Image, queue_size=10)
+#pub_depth_img_raw = rospy.Publisher('/sidewalk_detector/depth/image_raw',  Image, queue_size=10)
+pub_canny_edge = rospy.Publisher('/sidewalk_detector/color/canny_edge',  Image, queue_size=10)
+#pub_marker = rospy.Publisher('/sidewalk_detector/color/marker',  Image, queue_size=10)
+pub_depth_pts_in  = rospy.Publisher('/sidewalk_detector/depth/points_in',  PointCloud2, queue_size=10)
+pub_depth_pts_out = rospy.Publisher('/sidewalk_detector/depth/points_out', PointCloud2, queue_size=10)
+
+'''
 def Seperate_Cloud(cloud_points, mask_in):
 	pts_in, pts_out= [], []
 	for p in pc2.read_points(cloud_points, field_names = ("x", "y", "z"), skip_nans=True):
 		if p[2] > 0:
-			if mask_in[p[1], p[0]] == color_sidewalk_edge:
+			if mask_in[p[1], p[0]] == color_sidewalk:
 				pts_in.append([p[0], p[1], p[2]])	
 			else:
 				pts_out.append([p[0], p[1], p[2]])			
 	return create_cloud_xyz32(cloud_points.header, pts_in), create_cloud_xyz32(cloud_points.header, pts_out)
-
+'''
 
 class Sidewalk_detector:
-	def __init__(self, num_step):
+	def __init__(self, step_number):
+		global T_edge
+		global T_similar
 		self.counter = 0
-		self.num_step = num_step
+		self.step_number = step_number
 
 		sub_color_img_raw = message_filters.Subscriber('/camera/color/image_raw',Image)
 		sub_depth_img_raw = message_filters.Subscriber('/camera/depth/image_raw',Image)
@@ -59,6 +77,13 @@ class Sidewalk_detector:
 		ts.registerCallback(self.callback)
 
 	def Fill_gap(self, img_edge, w_box, h_box, x_box, y_box, y_up):
+	# Fill gap  between high-similarity ROI and closest edge
+	# Param 	img_edge 	: Canny edge image 
+	# Param 	w_box		: Width of ROI
+	# Param 	h_box		: height of ROI
+	# Param 	x_box		: x position of ROI
+	# Param 	y_box		: y position of ROI
+	# Param 	y_up		: search limit
 		lines_edge_gap = []
 		for x in xrange(x_box, x_box+w_box):
 			idx_edge = np.nonzero(img_edge[y_up:y_box+h_box, x])[0]
@@ -70,12 +95,18 @@ class Sidewalk_detector:
 		return lines_edge_gap
 	
 	def Compare_similarity(self, refBox, cropBox):
+	# Compute the similarity between two ROI
+	# Param 	refBox 	: ROI for reference
+	# Param 	cropBox : ROI for comparing
 		H1 = cv2.normalize(cv2.calcHist(refBox,  [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])).flatten()
 		H2 = cv2.normalize(cv2.calcHist(cropBox, [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])).flatten()
 		return cv2.compareHist(H1, H2, cv2.cv.CV_COMP_CORREL)
 
 
 	def callback(self, sub_color_img_raw, sub_depth_pts):
+		T_edge = rospy.get_param('T_edge')
+		T_similar = rospy.get_param('T_similar')
+
 		#### convert Image_msg to ndarry
 		try:
 			cv_color_img_raw = bridge.imgmsg_to_cv2(sub_color_img_raw, "bgr8")
@@ -98,7 +129,7 @@ class Sidewalk_detector:
 
 		#### Compute similarities
 		## Define ROI size
-		w_box, h_box = w/self.num_step, h/self.num_step
+		w_box, h_box = w/self.step_number, h/self.step_number
 
 		## Define reference ROI 
 		wr, hr = 200, 150
@@ -107,8 +138,8 @@ class Sidewalk_detector:
 		## Compute similarity for each ROI
 		similarities = []
 		c = 0
-		for j in xrange(self.num_step):
-			for i in xrange(self.num_step):
+		for j in xrange(self.step_number):
+			for i in xrange(self.step_number):
 				xc, yc, wc, hc = i*w_box, j*h_box, w_box, h_box
 				d = self.Compare_similarity(cv_color_img_raw[yr:yr+hr, xr:xr+wr], cv_color_img_raw[yc:yc+hc, xc:xc+wc])
 	
@@ -128,49 +159,49 @@ class Sidewalk_detector:
 		similarities_history.append(similarities)
 
 		### Search side walk edge
-		boxex_array = np.zeros(shape=(self.num_step,self.num_step,2), dtype=int)
-		similarities_array = np.zeros(shape=(self.num_step,self.num_step), dtype=float)
-		is_sidewalk = np.zeros(shape=(self.num_step,self.num_step), dtype=bool)
-		for y in xrange(self.num_step):
-			for x in xrange(self.num_step):
-				boxex_array[x,y] = similarities[(self.num_step*y) + x][0:2]
-				similarities_array[x,y] = similarities[(self.num_step*y) + x][4]
+		boxex_array = np.zeros(shape=(self.step_number,self.step_number,2), dtype=int)
+		similarities_array = np.zeros(shape=(self.step_number,self.step_number), dtype=float)
+		is_sidewalk = np.zeros(shape=(self.step_number,self.step_number), dtype=bool)
+		for y in xrange(self.step_number):
+			for x in xrange(self.step_number):
+				boxex_array[x,y] = similarities[(self.step_number*y) + x][0:2]
+				similarities_array[x,y] = similarities[(self.step_number*y) + x][4]
 
 		##  Leftward search
-		is_sidewalk[self.num_step/2-1, self.num_step-1] = True
-		for left in xrange(self.num_step/2-1):
-			idx_x, idx_y = self.num_step/2-2-left, self.num_step-1
+		is_sidewalk[self.step_number/2-1, self.step_number-1] = True
+		for left in xrange(self.step_number/2-1):
+			idx_x, idx_y = self.step_number/2-2-left, self.step_number-1
 			if (similarities_array[idx_x, idx_y] - similarities_array[idx_x+1, idx_y]) > T_edge: # if not an edge
 				is_sidewalk[idx_x, idx_y] = True
-			elif similarities_array[idx_x, idx_y] > T_edge_hp/2:
+			elif similarities_array[idx_x, idx_y] > T_similar/2:
 				is_sidewalk[idx_x, idx_y] = True
 			else:
 				break # if edge, break
 
 		##  Rightward search
-		is_sidewalk[self.num_step/2, self.num_step-1] = True
-		for right in xrange(self.num_step/2-1):
-			idx_x, idx_y = self.num_step/2+right+1, self.num_step-1
+		is_sidewalk[self.step_number/2, self.step_number-1] = True
+		for right in xrange(self.step_number/2-1):
+			idx_x, idx_y = self.step_number/2+right+1, self.step_number-1
 			if (similarities_array[idx_x, idx_y] - similarities_array[idx_x-1, idx_y]) > T_edge: # if not an edge
 				is_sidewalk[idx_x, idx_y] = True
-			elif similarities_array[idx_x, idx_y] > T_edge_hp/2:
+			elif similarities_array[idx_x, idx_y] > T_similar/2:
 				is_sidewalk[idx_x, idx_y] = True
 			else:
 				break # if edge, break
 
 		## Rpward search
 		lines_edge_gap = []
-		for idx_x in xrange(self.num_step):
-			if is_sidewalk[idx_x, self.num_step-1] == True:
-				for up in xrange(self.num_step-1):
-					idx_y = self.num_step-2-up
+		for idx_x in xrange(self.step_number):
+			if is_sidewalk[idx_x, self.step_number-1] == True:
+				for up in xrange(self.step_number-1):
+					idx_y = self.step_number-2-up
 					if (similarities_array[idx_x, idx_y] - similarities_array[idx_x, idx_y+1]) > T_edge: # if not an edge
 						is_sidewalk[idx_x, idx_y] = True
-					elif similarities_array[idx_x, idx_y] > T_edge_hp:
+					elif similarities_array[idx_x, idx_y] > T_similar:
 						is_sidewalk[idx_x, idx_y] = True
 					else:
 						y = idx_y
-						while y > 0 and similarities_array[idx_x, y] > T_edge_hp/3:
+						while y > 0 and similarities_array[idx_x, y] > T_similar/3:
 							y -=1
 						y_up = boxex_array[idx_x, y][1]
 						x_box, y_box = boxex_array[idx_x, idx_y][0], boxex_array[idx_x, idx_y][1]
@@ -183,17 +214,21 @@ class Sidewalk_detector:
 			for y in xrange(is_sidewalk.shape[1]):
 				if is_sidewalk[x,y] == True:
 					box = boxex_array[x,y]
-					img_mask_sidewalk[box[1]:box[1]+h_box, box[0]:box[0]+w_box]=color_sidewalk_edge
+					img_mask_sidewalk[box[1]:box[1]+h_box, box[0]:box[0]+w_box]=color_sidewalk
 		
 		for pts in lines_edge_gap:
-			cv2.line(img_mask_sidewalk, pts[0], pts[1], color_sidewalk_edge, thickness=1)
+			cv2.line(img_mask_sidewalk, pts[0], pts[1], color_sidewalk, thickness=1)
 
 		cv_img_marker = cv2.addWeighted(img_mask_sidewalk, 0.5, cv_color_img_raw, 0.5, 0)
 		#cv2.rectangle(cv_img_marker, (xr,yr),(xr+wr,yr+hr),(255,0,0),1)
-		#cv2.rectangle(cv_img_marker, (xc,yc),(xc+wc,yc+hc),color_sidewalk_edge,1)
-		#cv2.putText(cv_img_marker,str("%.2f"%d),(xc,yc+12), cv2.FONT_HERSHEY_SIMPLEX, 0.4,color_sidewalk_edge,1)
+		#cv2.rectangle(cv_img_marker, (xc,yc),(xc+wc,yc+hc),color_sidewalk,1)
+		#cv2.putText(cv_img_marker,str("%.2f"%d),(xc,yc+12), cv2.FONT_HERSHEY_SIMPLEX, 0.4,color_sidewalk,1)
 
 		### seperate point cloud
+		# Since Python does not have PCL API, fail to seperate 
+		# point cloud in an efficient way. Should use C++
+		pts_in, pts_out = sub_depth_pts, sub_depth_pts 
+
 		'''
 		h,w = img_mask_sidewalk.shape[:2]
 		M = cv2.getRotationMatrix2D((w/2,h/2),180,1)
@@ -212,6 +247,8 @@ class Sidewalk_detector:
 
 		### publish topics
 		pub_color_img_raw.publish(imgmsg_image)
+		pub_depth_pts_in.publish(pts_in)  
+		pub_depth_pts_out.publish(pts_out) 
 		pub_canny_edge.publish(imgmsg_canny_edge)
 
 		self.counter += 1
@@ -219,6 +256,6 @@ class Sidewalk_detector:
 if __name__ == '__main__':
 	rospy.init_node('sidewalk_detector')
 
-	detector = Sidewalk_detector(num_step)
+	detector = Sidewalk_detector(step_number)
 
 	rospy.spin()
